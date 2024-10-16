1c8467a7dbbe1730e9166e6f4687e29a88f07349
LuisL <dev.eng.luisluna@gmail.com>
2024-10-16 17:11:45 +0000

    version creada la Traficommand

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -1,33 +1,30 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const popupWhatsApp = () => {
-        const btnClosePopup = document.querySelector('.closePopup');
-        const btnOpenPopup = document.querySelector('.whatsapp-button');
-        const popup = document.querySelector('.popup-whatsapp');
-        const sendBtn = document.getElementById('send-btn');
+popupWhatsApp = () => {
 
-        // Cerrar el popup
-        btnClosePopup.addEventListener("click", () => {
-            popup.classList.remove('is-active-whatsapp-popup');
-        });
+    let btnClosePopup = document.querySelector('.closePopup');
+    let btnOpenPopup = document.querySelector('.whatsapp-button');
+    let popup = document.querySelector('.popup-whatsapp');
+    let sendBtn = document.getElementById('send-btn');
 
-        // Abrir el popup
-        btnOpenPopup.addEventListener("click", () => {
-            popup.classList.add('is-active-whatsapp-popup');
-            popup.style.animation = "fadeIn .6s 0.0s both";
-        });
+    btnClosePopup.addEventListener("click", () => {
+        popup.classList.toggle('is-active-whatsapp-popup')
+    })
 
-        // Enviar mensaje
-        sendBtn.addEventListener("click", () => {
-            const msg = document.getElementById('whats-in').value;
-            const relmsg = encodeURIComponent(msg);
-            window.open('https://wa.me/+56955212659?text=' + relmsg, '_blank');
-        });
+    btnOpenPopup.addEventListener("click", () => {
+        popup.classList.toggle('is-active-whatsapp-popup')
+        popup.style.animation = "fadeIn .6s 0.0s both";
+    })
 
-        // Abrir automáticamente después de 3 segundos
-        setTimeout(() => {
-            popup.classList.add('is-active-whatsapp-popup');
-        }, 3000);
-    }
+    sendBtn.addEventListener("click", () => {
+        let msg = document.getElementById('whats-in').value;
+        let relmsg = msg.replace(/ /g, "%20");
 
-    popupWhatsApp();
-});
+        window.open('https://wa.me/+56987713117?text=' + relmsg, '_blank');
+
+    });
+
+    setTimeout(() => {
+        popup.classList.toggle('is-active-whatsapp-popup');
+    }, 3000);
+}
+
+popupWhatsApp();
